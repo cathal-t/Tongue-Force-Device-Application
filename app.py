689a9ab4a75e28aae260e57ed1569f05b09f1d65
cathal-t <133865580+cathal-t@users.noreply.github.com>
2024-09-16 20:14:50 +0100

    Update app.py
    Auto calibration update

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from dash import dcc, html, Dash, Input, Output
 from layouts import max_force, calibration, arcade, home
 from callbacks import max_force_callbacks, navigation_callbacks, arcade_callbacks, calibration_callbacks  # Import callbacks
-
 import os
 
-# Ensure the 'profiles' directory exists
-if not os.path.exists('profiles'):
-    os.makedirs('profiles')
-
+# Import additional modules
+import os
 
 app = Dash(__name__)
+
+def load_calibration_data():
+    try:
+        filename = os.path.join(os.path.dirname(__file__), 'calibration_data.txt')
+        with open(filename, 'r') as f:
+            data = f.read().strip()
+            if data:
+                calibration_slope, calibration_intercept = map(float, data.split(','))
+                return {'slope': calibration_slope, 'intercept': calibration_intercept}
+    except (FileNotFoundError, ValueError):
+        pass
+    return {}
 
 # Define the main layout with dcc.Location to handle page navigation
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
+    # Shared Store for calibration coefficients
+    dcc.Store(id='shared-calibration-coefficients', data=load_calibration_data()),
     html.Div(id='page-content')
 ])
 
