a6eea9c4ae9f356398ac7ea4bd1538b9ceb1498f
cathal-t <133865580+cathal-t@users.noreply.github.com>
2024-09-16 19:51:46 +0100

    Update app.py
    Working calibration (without auto updating)

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from dash import dcc, html, Dash, Input, Output
 from layouts import max_force, calibration, arcade, home
 from callbacks import max_force_callbacks, navigation_callbacks, arcade_callbacks, calibration_callbacks  # Import callbacks
 
+import os
+
+# Ensure the 'profiles' directory exists
+if not os.path.exists('profiles'):
+    os.makedirs('profiles')
+
+
 app = Dash(__name__)
 
 # Define the main layout with dcc.Location to handle page navigation
