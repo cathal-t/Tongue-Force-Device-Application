954c49025e9115f13fe74abbc8f0e9bc5f6c6f07
cathal-t <133865580+cathal-t@users.noreply.github.com>
2024-09-25 18:56:13 +0100

    Update and rename home.py to layouts/home.py
    Updates as of 25/09/2024

--- layouts/home.py
+++ layouts/home.py
@@ -3,11 +3,17 @@ from dash import html
 layout = html.Div([
     html.H1('Dashboard Menu', style={'textAlign': 'center', 'color': '#007BFF'}),
     html.Div([
-        html.Button('Max Force Test', id='max-force-button', n_clicks=0, 
+        html.Button('Max Force Test', id='max-force-button', n_clicks=0,
                     style={'width': '300px', 'height': '100px', 'font-size': '24px', 'margin': '10px'}),
-        html.Button('Calibration', id='calibration-button', n_clicks=0, 
+        html.Button('Calibration', id='calibration-button', n_clicks=0,
                     style={'width': '300px', 'height': '100px', 'font-size': '24px', 'margin': '10px'}),
-        html.Button('Arcade', id='arcade-button', n_clicks=0, 
+        html.Button('Arcade', id='arcade-button', n_clicks=0,
                     style={'width': '300px', 'height': '100px', 'font-size': '24px', 'margin': '10px'}),
-    ], style={'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center', 'justify-content': 'center', 'height': '60vh'})
+    ], style={
+        'display': 'flex',
+        'flexDirection': 'column',
+        'alignItems': 'center',
+        'justify-content': 'center',
+        'height': '60vh'
+    })
 ])
